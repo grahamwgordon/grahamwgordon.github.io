a4522495e1f41c7cc19128b5c546cac4e2d334b0
grahamwgordon <grahamwalkergordon@gmail.com>
2023-02-02 11:06:28 -0800

    slideshow

--- my_script.js
+++ my_script.js
@@ -30,9 +30,11 @@ function copyToClipBoard() {
 }
 
 
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
+//window.onscroll = function() {myFunction()};
+
+//var navbar = document.getElementById("navbar");
+var navbar = document.getElementsByClassName("topnav")[0] ;
 var sticky = navbar.offsetTop;
 
 function myFunction() {
@@ -42,3 +44,34 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
