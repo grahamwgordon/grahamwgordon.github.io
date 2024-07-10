422d358ca36139252a2fb24e26dd5b17156533f6
graham <grahamwalkergordon@gmail.com>
2024-07-09 17:56:27 -0700

    bday

--- happybirthday/sketch.js
+++ happybirthday/sketch.js
@@ -1,15 +1,29 @@
+let s = 100 ;
+let t = 12 ;
+let f ;
+
+function preload() {
+    f = loadFont('rubik_fax.ttf') ;
+}
+
 function setup() {
     createCanvas(710, 400, WEBGL);
     angleMode(DEGREES);
-    strokeWeight(5);
-    noFill();
-  }
+    strokeWeight(1) ;
+    fill(250,200,200) ;
+    textFont(f) ;
+    textSize(t) ;
+}
   
-  function draw() {
+function draw() {
     background(222);
-  
-    // Call every frame to adjust camera based on mouse/touch
+
     orbitControl();
-  
-    box() ;
-  }
+
+    box(s) ;
+
+    push() ;
+    translate(s,0,0) ;
+    text('howdy',0,0) ;
+    pop() ;
+}
