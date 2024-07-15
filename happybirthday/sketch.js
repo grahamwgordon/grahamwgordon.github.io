d5b78c01c414b72db4d51d7a60d51562650c994c
graham <grahamwalkergordon@gmail.com>
2024-07-15 13:28:47 -0700

    party

--- happybirthday/sketch.js
+++ happybirthday/sketch.js
@@ -1,5 +1,6 @@
-let s = 100 ;
+let s = 200, t = 100 , z = 64;
 let img_when, img_where, img_what, img_food, img_byob, img_no ;
+let f ;
 
 function preload(){
     img_when  = loadImage('imgs/bday_31_cube_datetime.png') ;
@@ -8,12 +9,17 @@ function preload(){
     img_food  = loadImage('imgs/bday_31_cube_food.png') ;
     img_byob  = loadImage('imgs/bday_31_cube_byob.png') ;
     img_no    = loadImage('imgs/bday_31_cube_nogifts.png') ;
+
+    f = loadFont('rubik_fax.ttf') ;
 }
 
 function setup() {
-    createCanvas(666, 666, WEBGL) ;
+    createCanvas(333, 333, WEBGL) ;
     noStroke() ;
     noFill() ;
+    textFont(f) ;
+    textAlign(CENTER) ;
+    textSize(64)
 }
 
 function draw() {
@@ -23,42 +29,43 @@ function draw() {
 
     push() ;
         rotateX(0) ;
-        translate(0,0,s) ;
+        translate(0,0,t) ;
         texture(img_what) ;
+        text('howdy!') ;
         plane(s,s) ;
     pop() ;
 
     push() ;
         rotateX(HALF_PI) ;
-        translate(0,0,s) ;
+        translate(0,0,t) ;
         texture(img_when) ;
         plane(s,s) ;
     pop() ;
 
     push() ;
         rotateX(PI) ;
-        translate(0,0,s) ;
+        translate(0,0,t) ;
         texture(img_where) ;
         plane(s,s) ;
     pop() ;
 
     push() ;
         rotateX(3*HALF_PI) ;
-        translate(0,0,s) ;
+        translate(0,0,t) ;
         texture(img_no) ;
         plane(s,s) ;
     pop() ;
 
     push() ;
         rotateY(HALF_PI) ;
-        translate(0,0,s) ;
+        translate(0,0,t) ;
         texture(img_byob) ;
         plane(s,s) ;
     pop() ;
 
     push() ;
         rotateY(-HALF_PI) ;
-        translate(0,0,s) ;
+        translate(0,0,t) ;
         texture(img_food) ;
         plane(s,s) ;
     pop() ;
