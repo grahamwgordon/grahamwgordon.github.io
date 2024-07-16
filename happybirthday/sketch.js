b54d78245800f576f53fb015ddc4f91121ac4796
graham <grahamwalkergordon@gmail.com>
2024-07-15 22:23:04 -0700

    bday

--- happybirthday/sketch.js
+++ happybirthday/sketch.js
@@ -2,18 +2,20 @@ let s = 200, t = 100 , z = 64;
 let img_when, img_where, img_what, img_food, img_byob, img_no, img_mod ;
 let f ;
 let m ;
+let h ;
 
 function preload(){
-    img_when  = loadImage('imgs/bday_31_cube_datetime.png') ;
-    img_where = loadImage('imgs/bday_31_cube_loc.png') ;
-    img_what  = loadImage('imgs/bday_31_cube_party.png') ;
-    img_food  = loadImage('imgs/bday_31_cube_food.png') ;
-    img_byob  = loadImage('imgs/bday_31_cube_byob.png') ;
-    img_no    = loadImage('imgs/bday_31_cube_nogifts.png') ;
-    img_mod   = loadImage('zebra_texture.png') ;
+    img_when  = loadImage('assets/imgs/bday_31_cube_datetime.png') ;
+    img_where = loadImage('assets/imgs/bday_31_cube_loc.png') ;
+    img_what  = loadImage('assets/imgs/bday_31_cube_party.png') ;
+    img_food  = loadImage('assets/imgs/bday_31_cube_food.png') ;
+    img_byob  = loadImage('assets/imgs/bday_31_cube_byob.png') ;
+    img_no    = loadImage('assets/imgs/bday_31_cube_nogifts.png') ;
+    img_mod   = loadImage('assets/imgs/zebra_texture.png') ;
 
-    f = loadFont('rubik_fax.ttf') ;
-    m = loadModel('zebra.obj', true) ;
+    f = loadFont('assets/fonts/rubik_fax.ttf') ;
+    m = loadModel('assets/objs/zebra.obj', true) ;
+    h = loadModel('assets/objs/heart.obj', true) ;
 }
 
 function setup() {
@@ -30,6 +32,11 @@ function draw() {
     
     orbitControl() ;
     
+    push() ;
+        scale(1) ;
+        model(h) ;
+    pop() ;
+
     push() ;
         scale(0.666) ;
         texture(img_mod) ;
